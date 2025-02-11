22388d02cfd0f6298eab03ab62a5002e89e51d62
jupitorSendsme <jupitorsendsme@gmail.com>
2025-02-11 14:33:43 +0900

    delete expressjs_one_client & expressjs_one_server for setting git submodule

--- expressjs_one_server/services/indexService.js
+++
@@ -1,7 +0,0 @@
-exports.getIndexData = async () => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve({ message: 'Hello from Service Layer!' });
-		}, 500); // Simulate data fetching delay
-	});
-}; 
